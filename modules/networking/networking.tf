b2fbb71e89ff924e0429740d66761fe0f9170c69
ogdmerlin <ogdmerlin@gmail.com>
2024-01-13 13:06:40 +0000

    Networking

---
+++ modules/networking/networking.tf
@@ -0,0 +1,36 @@
+# resource block for networking
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.public_subnet_cidrs[0]
+  tags = {
+    Name = "${var.vpc_name}-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = ""
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-public-route-table"
+  }
+}
+
+
---
+++ modules/networking/variables.tf
@@ -0,0 +1,17 @@
+# variable for our networking resource block
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "The CIDR blocks for the public subnets"
+  type        = list(string)
+}
+
